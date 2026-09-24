e928cb18e8ce96d09e7ac9925d24047bc32ba684
Robert Roach <rjayroach@gmail.com>
2026-09-24 16:25:56 -0400

    deprecated in favor of bash script pcm

--- psm/lib/psm.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "psm/version"
-
-module Psm
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- psm/lib/psm/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Psm
-  VERSION = "0.1.0"
-end
